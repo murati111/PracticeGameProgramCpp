ba9c4a82121182f6db729ace68cbae2ffafa42a9
murati111 <murati111taisyo@gmail.com>
2019-04-20 23:30:16 +0900

    【Change】Actorにゲッターセッターを追加

--- WorkSpace/Chapter02/Actor.h
+++ WorkSpace/Chapter02/Actor.h
@@ -28,7 +28,17 @@ public:
 	virtual void UpdateActor(float deltaTime);
 
 	//ゲッター/セッター
+	const Vector2& GetPosition() const { return mPosition; }
+	void SetPosition(const Vector2& pos) { mPosition = pos; }
+	float GetScale() const { return mScale; }
+	void SetScale(float scale) { mScale = scale; }
+	float GetRotation() const { return mRotation; }
+	void SetRotation(float rotation) { mRotation = rotation; }
 
+	State GetState() const { return mState; }
+	void SetState(State state) { mState = state; }
+
+	class Game* GetGame() { return mGame; }
 	//コンポーネントの追加/削除
 	void AddComponent(class Component* component);
 	void RemoveComponent(class Component* component);
